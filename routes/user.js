e3a11be78911ad7893cd5381ea11441ec779ef33
Alex Otoo <alexotoous@outlook.com>
2020-06-04 18:08:09 +0000

    initial token

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const User = require("../models/user");
 
@@ -47,6 +49,7 @@ router.post("/signup", (req, res, next) => {
 });
 
 router.post("/login", (req, res, next) => {
+  //User.find() returns and array containing ONLY the found user object
   User.find({ email: req.body.email })
     .exec()
     .then((user) => {
@@ -58,12 +61,23 @@ router.post("/login", (req, res, next) => {
       bcrypt.compare(req.body.password, user[0].password, (err, result) => {
         if (err) {
           return res.status(401).json({
-            message: "auth failed",
+            message: "Auth failed",
           });
         }
         if (result) {
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId: user[0]._id,
+            },
+            process.env.JWT_KEY,
+            {
+              expiresIn: "1h",
+            }
+          );
           return res.status(200).json({
             mesaage: "Auth successful",
+            token: token,
           });
         }
         res.status(401).json({
